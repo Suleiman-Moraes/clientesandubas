128e86ddebddafbebedcc7fce7de4d199ca4bbab
valfredoqs <valfredosilva@gmail.com>
2018-11-13 22:02:57 -0200

    PutJsonUpdateTipoProduto

---
+++ src/main/java/br/com/senaigo/clientesandubas/PutJsonUpdateTipoProduto.java
@@ -0,0 +1,38 @@
+package br.com.senaigo.clientesandubas;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import br.com.senaigo.clientesandubas.model.TipoProduto;
+
+public class PutJsonUpdateTipoProduto {
+
+	public static void main(String[] args) {
+
+		Client client = Client.create();
+
+		WebResource webResource = client.resource("http://localhost:8080/tipoproduto");
+
+		// Data send to web service.
+		TipoProduto objeto = new TipoProduto(new Long(10), "teste nomex", "Teste descricao");
+
+		ClientResponse response = webResource.type("application/json").put(ClientResponse.class, objeto);
+
+		if (response.getStatus() != 200) {
+			System.out.println("Failed : HTTP error code : " + response.getStatus());
+
+			String error = response.getEntity(String.class);
+			System.out.println("Error: " + error);
+			return;
+		}
+
+		System.out.println("Output from Server .... \n");
+
+		TipoProduto output = response.getEntity(TipoProduto.class);
+
+		System.out.println(output.getNome()+ " ### " + output.getDescricao());
+
+	}
+
+}
